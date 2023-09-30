44e9e3fd86d70a0a97dea1e5803c7a33e6d6e39d
Christina Giancola <christinamgiancola@gmail.com>
2023-09-29 20:46:53 -0400

    [bug][invalid-token] Next inside try block
    When an invalid token is attempted to be parsed, if fails because the middleware function attempts to continue on.
    
    Solution
    Move next inside try block
    
    Note

--- server/src/middlewares.js
+++ server/src/middlewares.js
@@ -1,16 +1,16 @@
-const { verify } = require("./helpers");
+const { verify } = require('./helpers');
 exports.isAuthenticated = (req, res, next) => {
-  if (!req.cookies) {
-    throw "The request didn't have any cookies";
-  } else if (!req.cookies.activeUser || !req.cookies.accessToken) {
-    throw "Missing activeUser or accessToken in cookies";
-  } else {
-    try {
-      verify(req.cookies.activeUser, req.cookies.accessToken, { value: null });
-    } catch (e) {
-      console.log(e);
-      res.status(403).send({ error: e._message });
-    }
-  }
-  next();
+	if (!req.cookies) {
+		throw "The request didn't have any cookies";
+	} else if (!req.cookies.activeUser || !req.cookies.accessToken) {
+		throw 'Missing activeUser or accessToken in cookies';
+	} else {
+		try {
+			verify(req.cookies.activeUser, req.cookies.accessToken, { value: null });
+			next();
+		} catch (e) {
+			console.log(e);
+			res.status(403).send({ error: e._message });
+		}
+	}
 };
